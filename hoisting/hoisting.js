e568b3ef17275b87cef99e67a6d8dec358898cb4
asifhjahid <dhrubo8211diu@gmail.com>
2021-08-24 22:28:33 +0600

    hoisting

---
+++ hoisting/hoisting.js
@@ -0,0 +1,36 @@
+
+ console.log(myName);
+ myName();
+ console.log(a);
+ var a = 9;
+
+function myName(){
+     console.log(`JavaScript is awesome!`);
+ }
+
+
+myName()
+console.log(a);
+console.log(myName);
+
+
+
+
+var a = 10;
+
+ function myName2(){
+     console.log(`JavaScript is awesome!`);
+ }
+
+var myName=()=>{
+    console.log(`JavaScript is awesome!`);
+}
+
+// var a = 10;
+
+//  function myName2(){
+//      console.log(`JavaScript is awesome!`);
+//  }
+//  myName2()
+// console.log(a);
+// console.log(myName2);
