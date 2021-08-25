f00452ddfb21528892c2073bfae30b2fb918f357
asifhjahid <dhrubo8211diu@gmail.com>
2021-08-25 16:22:22 +0600

    undefined vs not defined

---
+++ undefined vs not defined/undefined-vs-not-defined.js
@@ -0,0 +1,19 @@
+// console.log(a); // a is undefined
+// var a = 4; 
+
+// console.log(x);// x is not define
+
+var a;
+console.log(a);
+
+if(a === undefined){
+    console.log(`a is undefined`);
+}
+else{
+    console.log(`a is not undefined`);
+}
+
+//console undefined
+
+var x = undefined;
+console.log(x);
