6a9f047942f934aefc6b3715b5e12247a5fa7afb
asifhjahid <dhrubo8211diu@gmail.com>
2021-08-31 19:16:17 +0600

    recursive function

--- basic js/basic.js
+++ basic js/basic.js
@@ -79,6 +79,11 @@ checkLeapYear(2052);
 checkLeapYear(2030);
 
 
+
+//iterative way
+
+
+
 //factorial
 
 // 5!=5*4*3*2*1
@@ -86,21 +91,23 @@ checkLeapYear(2030);
 
 //using for loop
 
-// let factorial = 1;
-// for (let i = 1; i <= 5; i++) {
-//   factorial = factorial * i;
-//   console.log(i, factorial);
-// }
+let factorial = 1;
+for (let i = 1; i <= 5; i++) {
+  factorial = factorial * i;
+  console.log(i, factorial);
+}
 
 //useing while loop
 
-let factorial = 1;
+let fact = 1;
 let i = 1;
 while(i<=5){
-    factorial = factorial * i;
-    console.log(i,factorial);
+    fact = fact * i;
+    console.log(i,fact);
     i++;
 }
+
+
 
 //useing function
 
@@ -115,3 +122,28 @@ function factorialValue(n) {
 
 let result = factorialValue(5);
 console.log(result);
+
+
+
+
+//recursive way
+// 0! = 1;
+// 4! = 1*2*3*4
+// 5! = 4!*5 || (5-1)*5
+// n! = (n-1)*n
+// 5! = 5*4*3*2*1 | 1*2*3*4*5
+
+
+
+function recursiveFact(n){
+    if(n==0){
+        return 1
+    }
+    else{
+        return n * recursiveFact(n-1)
+    }
+}
+
+let finalResult = recursiveFact(5)
+console.log(finalResult);
+
